90ee9085da230d4fc588fab14ddad44cbf61e1da
haru <yuusyasiratama@outlook.com>
2023-06-22 08:16:27 +0900

    脱出用mockに修正

--- App.js
+++ App.js
@@ -5,18 +5,6 @@ import Home from './component/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- component/Home.js
+++ component/Home.js
@@ -1,113 +1,43 @@
 import React, { Component } from 'react';
 import { Alert, Button, Text, TextInput, StyleSheet, View, SafeAreaView, RefreshControl } from 'react-native';
-import Message from './Message';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { StatusBar } from 'expo-status-bar';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-reanimated-table';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      note: '',
-      count: '',
-      items: [],
+      tableHead: ['', 'Head1', 'Head2', 'Head3'],
+      tableTitle: ['Title', 'Title2', 'Title3', 'Title4'],
+      tableData: [
+        ['1', '2', '3'],
+        ['a', 'b', 'c'],
+        ['1', '2', '3'],
+        ['a', 'b', 'c']
+      ]
     };
-
-    this.onload = async () => {
-      //全件取得
-      try {
-        let values = [];
-        const keys = await AsyncStorage.getAllKeys();
-        console.log("kokoha?", keys);
-        keys.sort().forEach(async (key) => {
-          if (key === "count") { return };
-          const value = await AsyncStorage.getItem(key.toString());
-          const json = {
-            key: key,
-            value: value,
-          }
-          console.log("valueeeeeeee:", value);
-          values.push(json);
-          console.log("json:::::", json);
-          this.setState({ items: values });
-        });
-
-      } catch (error) {
-        console.log('error', error);
-      }
-    }
-    this.onload();
-
-    this.doNote = (text) => {
-      this.setState({ note: text })
-      console.log("doNote:" + this.state.note);
-    };
-
-    this.doCount = (text) => {
-      this.setState({ count: text })
-    };
-
-    this.doPut = async () => {
-      try {
-        console.log("doPut");
-        console.log(this.state.items);
-        var count = await AsyncStorage.getItem('count');
-        if (count == null) { count = 1; }
-        console.log("count:::", count);
-
-        const note = this.state.note == null ? " " : this.state.note;
-        // const putJson = {
-        //   key:count,
-        //   value:note,
-        // };
-        console.log("note::::", note);
-        await AsyncStorage.setItem(count.toString(), note);
-        const nextCount = parseInt(count) + 1;
-        console.log("nextCount", nextCount);
-        await AsyncStorage.setItem('count', nextCount.toString());
-        this.doNote();
-        Alert.alert('put data!');
-        this.onload();
-      } catch (error) {
-        console.log(error);
-        Alert.alert(error);
-      }
-    }
-
-    this.clearAll = async () => {
-      try {
-        await AsyncStorage.clear()
-        this.setState({ items: [] })
-      } catch (e) {
-        // clear error
-      }
-      this.setState({
-        values: []
-      })
-      console.log('Clear Done.')
-    }
   }
 
   render() {
+    const state = this.state;
     return (
-      <SafeAreaView>
-        <View>
-          <TextInput
-            style={{
-              height: 40,
-              borderColor: 'gray',
-              borderWidth: 1,
-            }}
-            placeholder='note'
-            value={this.state.note}
-            onChangeText={this.doNote}
-          />
-        </View>
-        <Button title="PUT DATA" onPress={this.doPut} />
-        <Button title="CLEAR ALL" onPress={this.clearAll} />
-        {this.state.items.map(data => { return (<Text key={data.key}>{data.value}</Text>) })}
-
-      </SafeAreaView >
+      <View style={styles.container}>
+        <Table borderStyle={{ borderWidth: 1 }}>
+          <Row data={state.tableHead} flexArr={[1, 2, 1, 1]} style={styles.head} textStyle={styles.text} />
+          <TableWrapper style={styles.wrapper}>
+            <Col data={state.tableTitle} style={styles.title} heightArr={[28, 28]} textStyle={styles.text} />
+            <Rows data={state.tableData} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text} />
+          </TableWrapper>
+        </Table>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  wrapper: { flexDirection: 'row' },
+  title: { flex: 1, backgroundColor: '#f6f8fa' },
+  row: { height: 28 },
+  text: { textAlign: 'center' }
+});
